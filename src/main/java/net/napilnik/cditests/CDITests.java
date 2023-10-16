e95cc7cae77090c7c62f0e9cdf47aa75ae2c7615
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-10-16 15:52:10 +0300

    initial commit

---
+++ src/main/java/net/napilnik/cditests/CDITests.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.napilnik.cditests;
+
+/**
+ *
+ * @author malyshev
+ */
+public class CDITests {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
