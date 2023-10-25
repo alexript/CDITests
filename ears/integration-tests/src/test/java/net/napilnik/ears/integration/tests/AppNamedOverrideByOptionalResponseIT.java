9f5c2c066b6fcee090681a3e18348b55ff10d529
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-10-25 14:59:20 +0300

    restructure integrations tests

--- ears/integration-tests/src/test/java/net/napilnik/ears/integration/tests/AppNamedOverrideByOptionalResponseIT.java
+++ ears/integration-tests/src/test/java/net/napilnik/ears/integration/tests/AppNamedOverrideByOptionalResponseIT.java
@@ -1,4 +1,4 @@
-package net.napilnik.ears.app.named.tests.integration;
+package net.napilnik.ears.integration.tests;
 
 import java.io.File;
 import java.net.URI;
@@ -6,9 +6,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import org.jboss.arquillian.container.test.api.Deployer;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.as.arquillian.api.ContainerResource;
 import org.jboss.as.arquillian.container.ManagementClient;
 import org.jboss.shrinkwrap.api.Archive;
@@ -27,19 +29,28 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * @author malyshev
  */
 @ExtendWith(ArquillianExtension.class)
-public class AppNamedResponseIT {
+public class AppNamedOverrideByOptionalResponseIT {
 
     @Deployment(testable = false, name = "app-named")
     public static Archive<?> createAppNamedDeployment() {
-        return ShrinkWrap.createFromZipFile(EnterpriseArchive.class, new File("../app-named-ear/target/app-named-ear-1.0-SNAPSHOT.ear"));
+        return ShrinkWrap.createFromZipFile(EnterpriseArchive.class, new File("../app-named/app-named-ear/target/app-named-ear-1.0-SNAPSHOT.ear"));
+    }
+
+    @Deployment(testable = false, name = "app-optional", managed = false)
+    public static Archive<?> createAppOptionalDeployment() {
+        return ShrinkWrap.createFromZipFile(EnterpriseArchive.class, new File("../app-optional/app-optional-ear/target/app-optional-ear-1.0-SNAPSHOT.ear"));
     }
 
     @ContainerResource
     ManagementClient managementClient;
 
+    @ArquillianResource
+    Deployer deployer;
+
     @Test
     @RunAsClient
-    public void testBeanNamedDefault() throws Exception {
+    public void testBeanNamedOverride() throws Exception {
+
         URI baseUrl = managementClient.getWebUri();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest
@@ -48,6 +59,12 @@ public class AppNamedResponseIT {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         assertTrue(response.body().contains("I am BeanNamedDefault"));
 
-    }
+        deployer.deploy("app-optional");
 
+        response = client.send(request, BodyHandlers.ofString());
+        assertTrue(response.body().contains("I am BeanNamedDefault"));
+
+        deployer.undeploy("app-optional");
+
+    }
 }
